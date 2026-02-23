c524e9c114ad7e8b87f8d4c5a84f29d149d90f41
Tommy Jeff <threcius@yahoo.com>
2026-02-23 08:49:21 +0800

    Add NSApp class

--- src/cocoawebview.cr
+++ src/cocoawebview.cr
@@ -1,10 +1,27 @@
-@[Link(ldflags: "-framework Foundation #{__DIR__}/../ext/cocoawebview.m")]
+@[Link(ldflags: "-framework Cocoa -framework WebKit -framework Foundation #{__DIR__}/../ext/cocoawebview.m")]
 lib Native
   fun add(a : Int32, b : Int32) : Int32
+  fun nsapp_init : Void*
+  fun nsapp_run : Void
+  fun nsapp_exit : Void
 end
 
 module Cocoawebview
-  VERSION = "0.1.0"
+  class NSApp
+    @handle : Void*
+    def initialize
+      @handle = Native.nsapp_init()
+    end
+
+    def run
+      Native.nsapp_run
+    end
+
+    def exit
+      Native.nsapp_exit
+    end
+  end
+
   def self.add(a : Int32, b : Int32)
     Native.add(a, b)
   end
