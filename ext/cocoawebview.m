8a296a6fee472cbe37bd9f1d4a78227e00b9ac01
Tommy Jeff <threcius@yahoo.com>
2026-02-23 11:31:43 +0800

    Fixed set_on_webview_message() error

--- ext/cocoawebview.m
+++ ext/cocoawebview.m
@@ -222,6 +222,7 @@ typedef struct {
         //VALUE rb_body = rb_str_new_cstr(body);
         //rb_funcall(rb_cocoawebview, rb_intern("webview_msg_handler"), 1, rb_body);
         
+        NSLog(@"msg in ojbc!");
         if (on_webview_message_cb) on_webview_message_cb(body);
     }
 }
