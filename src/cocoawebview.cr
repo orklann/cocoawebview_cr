e551faa0e7859f2ede396101d46bc8b8fa9f85ed
Tommy Jeff <threcius@yahoo.com>
2026-02-24 10:27:01 +0800

    Fixed syntax error

--- src/cocoawebview.cr
+++ src/cocoawebview.cr
@@ -68,10 +68,11 @@ module Cocoawebview
 
   class NSMenu
     @handle : Void*
-    @bindings : Hash(Int32, ->())
+    @bindings : Hash(Int32, Proc(Nil))
 
     def initialize
       @handle = Native.nsmenu_initialize()
+      @bindings = {} of Int32 => Proc(Nil)
     end
 
     def create_menu_item_with(title : String, tag : Int32, key : String, &block : ->())
